2b1c83e082ecb99e77ebd887f525979c6c04e018
lisicase <casee@uw.edu>
2020-05-28 22:46:42 -0700

    added very basic elements: title, search, sort, footer, search results

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default class App extends Component {
   
@@ -11,7 +13,30 @@ export default class App extends Component {
         return (
             <>
             <NavBar />
-            <BookCard />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <h2 className="page-title">Explore</h2>
+                    </div>
+
+                    <div className="col">
+                        <SearchBar />
+                    </div>
+
+                    <div className="col">
+                        <SortButton />
+                    </div>
+                </div>
+            </div>
+
+            <div className="loading-icon">
+                <i className="d-none fa fa-spinner fa-spin fa-lg" aria-hidden="true"></i>
+            </div>
+
+            <SearchResults />
+
+            <hr></hr>
+            <Footer />
             </>
         );
     }
@@ -49,22 +74,79 @@ class NavLinks extends Component {
 
 class BookCard extends Component {
     render() {
+        return (
+            <Card id="search" className="text-center mb-8 border-lightgray mt-4" style={{ height: '500', width: '300' }}>
+                <Card.Body>
+                    <Card.Text></Card.Text>
+                    <Card.Img variant="top" src="img/book.png" alt="Book cover" className="rounded mt-3"/>
+                    <div className="card-body text-dark">
+                        <h4 className="card-booktitle">Title</h4>
+                        <h5 className="card-text card-author">Author Name</h5>
+                        <h6 className="card-text card-pages">Page Count</h6>
+                        <p className="card-text">Rating</p>
+                        <Button variant="primary">Learn More</Button>{' '}
+                    </div>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
 
-      return (
-        <Card id="search" className="mb-8 border-lightgray mt-4" style={{ height: '500', width: '300' }}>
-            <div className="text-center">
-                <p>Test</p>
-                <img src="img/book.png" alt="Book cover" className="rounded mt-3"/>
-                <div className="card-body text-dark">
-                    <h4 className="card-booktitle">Title</h4>
-                    <h5 className="card-text card-author">Author Name</h5>
-                    <h6 className="card-text card-pages">Page Count</h6>
-                    <p>Rating</p>
-                    <p></p>
-                    <Button variant="primary">Learn More</Button>{' '}
+class SearchResults extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="card-columns">
+                    <div className="search-results text-center">
+                        <BookCard />
+                    </div>
                 </div>
             </div>
-        </Card>
-      )
+        );
+    }
+}
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="search-box">
+                <div className="input-group mb-3">
+                    <input id="search-value" type="text" className="form-control"
+                        placeholder="Search books"/>
+                    <div className="input-group-append">
+                        <Button variant="primary" id="search-button" className="btn btn-secondary btn-small" type="submit">
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </Button>{' '}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <>
+            <footer>
+                <p>Copyright&copy; 2020 | Lisi Case and Greyson Fields</p>
+                <p>Contact: Lisi Case <a href="mailto:casee@uw.edu">casee@uw.edu</a> | Greyson Fields <a
+                        href="mailto:gfields5@uw.edu">gfields5@uw.edu</a></p>
+                <p>Data from <a href="https://developers.google.com/books/docs/overview">Google Books API</a></p>
+            </footer>
+            </>
+        );
+    }
+}
+
+class SortButton extends Component {
+    render() {
+        return (
+            <DropdownButton id="dropdown-basic-button" size="sm" variant="secondary" title="Sort">
+                <Dropdown.Item>Sort By Pages</Dropdown.Item>
+                <Dropdown.Item>Sort By Rating</Dropdown.Item>
+                <Dropdown.Item>Sort By Price</Dropdown.Item>
+            </DropdownButton>
+        );
     }
 }
