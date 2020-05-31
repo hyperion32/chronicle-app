c9ee2e36c622ec979111490f1470827a801d5ce1
lisicase <casee@uw.edu>
2020-05-31 13:58:02 -0700

    returned lists/log to not share components

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
-import PageHeaderWithAdd from './ReadingLog.js';
-import ListContainer from './List.js';
+//import ListContainer from './List.js';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 class ListsContainer extends Component {
     render() {
         return (
             <div className="container-fluid">
-                {/* <PageHeaderWithAdd title="Lists"/> */}
+                <div className="row">
+                    <div className="col">
+                        <div className="d-flex w-100">
+                            <h2 className="page-title">Lists</h2>
+                        </div>
+                        <div className="button-add">
+                            <Button variant="light" id="add-new-list" className="add-new btn btn-light btn-lg">+</Button>
+                        </div>
+                    </div>
+                </div>
 
-                <ListContainer>
-                    <ListItem title="Favorites" bookCount="6" color="lightblue"/>
-                    <ListItem title="Fun" bookCount="3" color="lightseagreen"/>
-                    <ListItem title="Honors 230 A: Leadership, Democracy, and a More
-                                Thoughtful Public" bookCount="20" color="plum"/>
-                    <ListItem title="Professional Development" bookCount="11" color="gold"/>
-                    <ListItem title="Recommended" bookCount="15" color="pink"/>
-                    <ListItem title="Work" bookCount="2" color="palevioletred"/>
-                </ListContainer>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div id="lists" className="col">
+                            <ListGroup variant="flush">
+                                <ListItem title="Favorites" bookCount="6" color="lightblue"/>
+                                <ListItem title="Fun" bookCount="3" color="lightseagreen"/>
+                                <ListItem title="Honors 230 A: Leadership, Democracy, and a More
+                                            Thoughtful Public" bookCount="20" color="plum"/>
+                                <ListItem title="Professional Development" bookCount="11" color="gold"/>
+                                <ListItem title="Recommended" bookCount="15" color="pink"/>
+                                <ListItem title="Work" bookCount="2" color="palevioletred"/>
+                            </ListGroup>
+                        </div>
+                        <div className="buffer col"></div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/ReadingLog.js
+++ src/ReadingLog.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import PageHeaderWithAdd from './ReadingLog.js';
-import ListContainer from './List.js';
+//import PageHeaderWithAdd from './ReadingLog.js';
+//import ListContainer from './List.js';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 class LogContainer extends Component {
     render() {
         return (
             <div className="container-fluid">
-                {/* <PageHeaderWithAdd title="Log" /> */}
+                <div className="row">
+                    <div className="col">
+                        <div className="d-flex w-100">
+                            <h2 className="page-title">Log</h2>
+                        </div>
+                        <div className="button-add">
+                            <Button variant="light" id="add-new-list" className="add-new btn btn-light btn-lg">+</Button>
+                        </div>
+                    </div>
+                </div>
 
-                <ListContainer>
-                    <Session title="Favorites" bookCount="6" color="lightblue" />
-                    <Session title="Fun" bookCount="3" color="lightseagreen" />
-                    <Session title="Honors 230 A: Leadership, Democracy, and a More
-                                Thoughtful Public" bookCount="20" color="plum" />
-                    <Session title="Professional Development" bookCount="11" color="gold" />
-                    <Session title="Recommended" bookCount="15" color="pink" />
-                    <Session title="Work" bookCount="2" color="palevioletred" />
-                </ListContainer>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div id="lists" className="col">
+                            <ListGroup variant="flush">
+                                <Session title="Favorites" bookCount="6" color="lightblue" />
+                                <Session title="Fun" bookCount="3" color="lightseagreen" />
+                                <Session title="Honors 230 A: Leadership, Democracy, and a More
+                                            Thoughtful Public" bookCount="20" color="plum" />
+                                <Session title="Professional Development" bookCount="11" color="gold" />
+                                <Session title="Recommended" bookCount="15" color="pink" />
+                                <Session title="Work" bookCount="2" color="palevioletred" />
+                            </ListGroup>
+                        </div>
+                        <div className="buffer col"></div>
+                    </div>
+                </div>
             </div>
         );
     }
