03d10d9cbab43a00aed32ddd4554af4d6506f6cb
hyperion32 <greysonfields32@gmail.com>
2020-05-31 14:42:36 -0700

    deleted button

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -6,7 +6,6 @@ const SearchResults = (props) => {
         <div className="search-results">
             <form onSubmit={props.data} action="">
                 <input onChange={props.handleSubmit} type="text" placeholder="Search"/>
-                {/*<button type="submit">Search</button>*/}
                 <Button variant="primary" id="search-button" type="submit">
                     <i className="fa fa-search" aria-hidden="true"></i>
                 </Button>{' '}
