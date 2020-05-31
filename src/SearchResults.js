b995e68a896bde5af95a5f7dd456849d1dd6643a
lisicase <casee@uw.edu>
2020-05-31 00:28:19 -0700

    changed search button to icon

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const SearchResults = (props) => {
     return (
         <div className="search-results">
             <form onSubmit= { props.data }action="">
                 <input onChange={props.handleSubmit} type="text" placeholder="Search"/>
-                <button type="submit">Search</button>
+                {/*<button type="submit">Search</button>*/}
+                <Button variant="primary" id="search-button" type="submit">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </Button>{' '}
             </form>
         </div>
     );
