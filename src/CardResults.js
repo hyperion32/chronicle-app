506c65ff2b4c2bcfefdc09e635d695b3efecd40c
hyperion32 <greysonfields32@gmail.com>
2020-05-31 18:17:02 -0700

    styled cards

--- src/CardResults.js
+++ src/CardResults.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 
 const CardResults = (props) => {
     return (
-        <div className="card-container">
-            <img src={props.image} alt="" />
-            <div className="card-body">
-                <h2 className="card-text">{props.title}</h2>
-                <h3 className="card-text">{props.author}</h3>
-                <p className="card-text">{props.pages} pages</p>
-                <p className="card-text">{props.rating} out of 5 stars</p>
-                <Button variant="primary" className="card-button" href={props.bookInfo}>Learn More</Button>{' '}
-                {/*<p className="card-text">ISBN10: {props.isbn10}</p>*/}
-                {/*<p className="card-text">ISBN13: {props.isbn13}</p>*/}
-            </div>
-        </div>
+        <CardDeck>
+            <Card>
+                <Card.Img variant="top" src={props.image} alt="" />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>{props.author}</Card.Text>
+                    <Card.Text>{props.pages} pages</Card.Text>
+                    <Card.Text>{props.rating} out of 5 stars</Card.Text>
+                    <Card.Text>{props.author}</Card.Text>
+                    <Button variant="primary" className="card-button" href={props.bookInfo}>Learn More</Button>{' '}
+                </Card.Body>
+            </Card>
+        </CardDeck>
     );
 }
 
