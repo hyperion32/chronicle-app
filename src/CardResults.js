225b31a909f43f5f3f87f36b9454d8576fd4742a
hyperion32 <greysonfields32@gmail.com>
2020-06-02 19:50:32 -0700

    hard coded the img, working on img from json

--- src/CardResults.js
+++ src/CardResults.js
@@ -6,13 +6,14 @@ const CardResults = (props) => {
     return (
         <div className="container-fluid">
             <Card className='text-center'>
-                <Card.Img variant="top" src={props.image} alt="" />
+                <Card.Img variant="top" src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="" />
                 <Card.Body>
                     <Card.Title>{props.title}</Card.Title>
                     <Card.Text>{props.author}</Card.Text>
                     <Card.Text>{props.pages} pages</Card.Text>
+                    {/* // add conditional info to pages and rating */}
                     <Card.Text>{props.rating} out of 5 stars</Card.Text>
-                    <Card.Text>{props.date}</Card.Text>
+                    <Card.Text>Published Date: {props.date === '0000' ? 'date unknown' : props.date.substring(0, 4)}</Card.Text>
                     {/* <Card.Text>{props.isbn10}</Card.Text>
                     <Card.Text>{props.isbn13}</Card.Text> */}
                     <Button variant="primary" className="card-button" href={props.bookInfo}>Learn More</Button>{' '}
