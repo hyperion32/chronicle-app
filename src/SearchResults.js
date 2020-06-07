2554178924eddecb510dba5a31af2da26bcf704f
hyperion32 <greysonfields32@gmail.com>
2020-06-07 09:51:04 -0700

    working on adding sign in to account page

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -23,32 +23,38 @@ class SearchResults extends React.Component {
     render() {
 
         return (
-            <div className="row">
-                <div className="col ">
-                    <h2>Explore</h2>
-                </div>
-                <div className="col">
-                    <div className="search-results">
-                        <form onSubmit={this.props.data} action="">
-                            <input onKeyUp={(e) => { this.setState({ searchVal: e.target.value }) }} type="text" placeholder="Search" />
-                            <Button onClick={this.submitSearch.bind(this)} variant="primary btn-sm" id="search-button" type="submit">
-                                <i className="fa fa-search" aria-hidden="true"></i>
-                            </Button>{' '}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col">
+                                <div className="d-flex w-100">
+                                    <h2 className="page-title">Explore</h2>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="search-results">
+                                    <form onSubmit={this.props.data} action="" className="mt-3">
+                                        <input onKeyUp={(e) => { this.setState({ searchVal: e.target.value }) }} type="text" placeholder="Search" />
+                                        <Button onClick={this.submitSearch.bind(this)} variant="primary btn-sm" id="search-button" type="submit">
+                                            <i className="fa fa-search" aria-hidden="true"></i>
+                                        </Button>{' '}
 
-                        </form>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <select defaultValue="Sort" onChange={this.props.handleSort} className="mt-4">
+                                    <option disabled value="Sort">Sort</option>
+                                    <option value="oldest">Oldest</option>
+                                    <option value="newest">Newest</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col">
-                    <select defaultValue="Sort" onChange={this.props.handleSort}>
-                        <option disabled value="Sort">Sort</option>
-                        {/* <option value="MostPages">Most Pages</option>
-                        <option value="LeastPages">Least Pages</option> */}
-                        <option value="oldest">Oldest</option>
-                        <option value="newest">Newest</option>
-                    </select>
+                    {/* <div className="buffer col"></div> */}
                 </div>
             </div>
-
         );
     }
 }
