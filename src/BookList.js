e67d9091a1def521ef66e6a38afb134eb8e20c95
hyperion32 <greysonfields32@gmail.com>
2020-06-08 12:39:05 -0700

    optimizing - searchresults still has bug during refactoring

--- src/BookList.js
+++ src/BookList.js
@@ -17,14 +17,12 @@ class BookCard extends React.Component {
 
     updateResults = (searchVal) => {
         this.setState({ searchInput: searchVal });
-        console.log(searchVal);
 
         fetch("https://www.googleapis.com/books/v1/volumes?q=" + searchVal)
 
             .then(res => res.json())
             .then(
                 (result) => {
-                    console.log(result)
                     this.setState({
                         isLoaded: true,
                         items: result.items
@@ -45,17 +43,14 @@ class BookCard extends React.Component {
     }
 
     handleSort = (e) => {
-        console.log(e.target.value)
         this.setState({ sortPageCount: e.target.value })
     }
 
     render() {
         const sorted = this.state.items.sort((a, b) => {
             if (this.state.sortPageCount === "newest") {
-                console.log("newest");
                 return parseInt(b.volumeInfo.publishedDate.substring(0, 4)) - parseInt(a.volumeInfo.publishedDate.substring(0, 4));
             } else if (this.state.sortPageCount === "oldest") {
-                console.log("oldest");
                 return parseInt(a.volumeInfo.publishedDate.substring(0, 4)) - parseInt(b.volumeInfo.publishedDate.substring(0, 4));
             }
 
@@ -77,8 +72,6 @@ class BookCard extends React.Component {
                         </div>
                     </div>
                 </>
-
-
             );
         }
     }
